0067248b2e5af20264ee1e372a9aa7003b30c2af
fujiokayu <fujiokayu@gmail.com>
2019-06-16 00:27:23 +0900

    implement arg parser.

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,35 @@ import (
 	"github.com/urfave/cli"
 )
 
+func defineApp(app *cli.App) {
+	app.Name = "goDownloader"
+	app.Usage = "file downloader made of Go."
+	app.Version = "1.0.0"
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "pallarel, p",
+			Value: 0,
+			Usage: "pallarel number to download file",
+		},
+		cli.StringFlag{
+			Name:  "url, u",
+			Usage: "file url to download",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		downloader.Download("WIP")
+		return nil
+	}
+
+}
+
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	defineApp(app)
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	downloader.Download("WIP")
 }
