70253737f0c015155c9b917e5a2793fa94d12c19
fujiokayu <fujiokayu@gmail.com>
2019-06-17 02:18:02 +0900

    Add function to get content-length.

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -1,14 +1,35 @@
 package downloader
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 )
 
-//Download
-func Download(url string) error {
+//GetContentLength
+func GetContentLength(url string) (err error, contentLength uint64) {
+	res, err := http.Head(url)
+	if err != nil {
+		return err, 0
+	}
+	if res.StatusCode != 200 {
+		return fmt.Errorf("http error: status code %d", res.StatusCode), 0
+	}
+	defer res.Body.Close()
+
+	length, err := strconv.ParseUint(res.Header.Get("Content-Length"), 0, 64)
+	if err != nil {
+		return err, 0
+	}
+
+	return nil, length
+}
+
+//RangeDownload
+func RangeDownload(url string, startPos int64, rangeByte int64) error {
 	res, err := http.Get(url)
 	if err != nil {
 		return err
