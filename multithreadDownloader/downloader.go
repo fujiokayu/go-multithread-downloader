8aeaf7b90206e64578024dc101cbaf95967682ad
fujiokayu <fujiokayu@gmail.com>
2019-06-19 13:10:25 +0900

    deal with range-access download. (non-parallel)

--- multithreadDownloader/downloader.go
+++ multithreadDownloader/downloader.go
@@ -1,79 +1,112 @@
-package downloader
+package multithreadDownloader
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
+	"runtime"
 	"strconv"
 )
 
 type DownlodeClient struct {
-	url            string
-	contentType    string
-	contentLength  uint64
-	responseHeader *http.Response
+	URL             string
+	ContentLength   int64
+	HasAcceptRanges bool
+	ThreadNumber    int
+	IsReady         bool
 }
 
-func (DownlodeClient *DownlodeClient) setResponceHeader() error {
-	fmt.Println("setResponceHeader of ", DownlodeClient.url)
-	res, err := http.Head(DownlodeClient.url)
+func (DownlodeClient *DownlodeClient) SetResponceHeader() error {
+	fmt.Println("setResponceHeader of ", DownlodeClient.URL)
+	res, err := http.Head(DownlodeClient.URL)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get Header: %s", err)
 	}
 	if res.StatusCode != 200 {
 		return fmt.Errorf("http error: status code %d", res.StatusCode)
 	}
 	defer res.Body.Close()
 
-	DownlodeClient.responseHeader = res
+	DownlodeClient.ContentLength, err = strconv.ParseInt(res.Header.Get("Content-Length"), 0, 64)
+	if err != nil {
+		return fmt.Errorf("failed to get content-length: %s", err)
+	}
+
+	DownlodeClient.HasAcceptRanges = (res.Header.Get("Accept-Ranges") == "bytes")
+	DownlodeClient.IsReady = DownlodeClient.HasAcceptRanges
+
 	return nil
 }
 
-func HasAcceptRanges(dc DownlodeClient) (error, bool) {
-	err := dc.setResponceHeader()
-	if err != nil {
-		return err, false
-	}
-	res := dc.responseHeader.Header.Get("Accept-Ranges")
+//Download startPos int64, rangeByte int64
+func (downlodeClient DownlodeClient) Download(threadNumber int) error {
 
-	return nil, res == "bytes"
-}
-
-//GetContentLength
-func GetContentLength(url string) (error, uint64) {
-	dc := &DownlodeClient{url, "", 0, nil}
-	err := dc.setResponceHeader()
-	if err != nil {
-		return err, 0
-	}
-	fmt.Println(HasAcceptRanges(*dc))
-	length, err := strconv.ParseUint(dc.responseHeader.Header.Get("Content-Length"), 0, 64)
-	if err != nil {
-		return err, 0
+	if threadNumber == 0 {
+		threadNumber = runtime.NumCPU()
 	}
 
-	return nil, length
-}
-
-//RangeDownload
-func RangeDownload(url string, startPos int64, rangeByte int64) error {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
+	if !downlodeClient.IsReady {
+		return fmt.Errorf("DownlodeClient is not ready")
 	}
-	//client := &http.Client{Timeout: time.Duration(10) * time.Second}
 
-	defer req.Body.Close()
+	fmt.Sprintf("download %d parallels", threadNumber)
+
+	payloadSize := downlodeClient.ContentLength / int64(threadNumber)
+	remaindSize := downlodeClient.ContentLength
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	// Create the file
-	out, err := os.Create(path.Base(url))
+	out, err := os.Create(path.Base(downlodeClient.URL))
 	if err != nil {
+		fmt.Println(err)
 		return err
 	}
 	defer out.Close()
 
+	for i := 0; remaindSize > 0; i++ {
+		fmt.Println("Go: ", remaindSize)
+		startPos := downlodeClient.ContentLength - remaindSize
+		endPos := startPos + payloadSize
+		if endPos > downlodeClient.ContentLength {
+			endPos = downlodeClient.ContentLength
+		}
+		remaindSize -= payloadSize
+
+		req, err := http.NewRequest("GET", downlodeClient.URL, nil)
+		if err != nil {
+			//				return err
+		}
+		req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", startPos, endPos))
+		req = req.WithContext(ctx)
+		fmt.Println(i, ": done")
+
+		var client http.Client
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Do i: ", err)
+		}
+		defer res.Body.Close()
+		//var buf bytes.Buffer
+		_, err = io.Copy(out, res.Body)
+		if err != nil {
+			fmt.Println("Copy i: ", err)
+		}
+
+		// Write the body to file
+		_, err = io.Copy(out, res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+	}
+
 	// Write the body to file
 	//_, err = io.Copy(out, res.Body)
+	fmt.Println("done")
 	return err
 }
